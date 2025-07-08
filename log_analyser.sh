b7a5d550539fb1487ad3f71ce6babeed2d3cc7d4
KhalidIbra <khalidrk@hotmail.co.uk>
2025-07-08 16:31:12 +0100

    edited to reference log file as environment variable

--- log_analyser.sh
+++ log_analyser.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
+export LOGFILE=nginx-access.log
 
 echo -e "\nTop 5 IP addresses with the most requests"
-awk '{print $1}' nginx-access.log | sort | uniq -c | sort -nr | head -5 | awk '{print $2, "-", $1, "requests"}'
+awk '{print $1}' "$LOGFILE" | sort | uniq -c | sort -nr | head -5 | awk '{print $2, "-", $1, "requests"}'
 
 
 
 echo -e "\nTop 5 most requested paths"
-awk '{print $7}' nginx-access.log | sort | uniq -c | sort -nr | head -5 | awk '{print $2, "-", $1, "requests"}'
+awk '{print $7}' "$LOGFILE" | sort | uniq -c | sort -nr | head -5 | awk '{print $2, "-", $1, "requests"}'
 
 
 
 echo -e "\nTop 5 response status codes"
-awk '{print $9}' nginx-access.log | sort | uniq -c | sort -nr | head -5 | awk '{print $2, "-", $1, "requests"}'
+awk '{print $9}' "$LOGFILE" | sort | uniq -c | sort -nr | head -5 | awk '{print $2, "-", $1, "requests"}'
